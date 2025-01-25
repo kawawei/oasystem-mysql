cfac5a877cda3d608224debe54cb5015f56ccde7
kawa <weihomeben@gmail.com>
2025-01-26 01:57:46 +0800

    fix: simplify CORS configuration and add necessary headers

--- server/src/app.js
+++ server/src/app.js
@@ -11,24 +11,12 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: function(origin, callback) {
-    // 允許沒有 origin 的請求（例如本地請求）
-    if (!origin) return callback(null, true);
-    
-    // 檢查 origin 是否在允許列表中
-    const allowedOrigins = process.env.NODE_ENV === 'production'
-      ? ['http://localhost:3000', 'http://13.250.109.239', 'https://oasystem.lihengtech.com.tw']
-      : ['http://localhost:3000'];
-      
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: true, // 允許所有來源，我們將在生產環境中通過 Nginx 來控制
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Content-Length', 'Content-Type'],
+  maxAge: 86400 // 預檢請求的結果可以被快取 24 小時
 }));
 
 app.use(express.json());
