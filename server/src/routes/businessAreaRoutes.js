54b36c4eff57d48af50e525d172b5c7f5669ee1c
kawa <weihomeben@gmail.com>
2025-03-29 06:48:41 +0800

    fix: 修正業務區域路由的認證中間件 / Fix authentication middleware in business area routes

--- server/src/routes/businessAreaRoutes.js
+++ server/src/routes/businessAreaRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const { getBusinessUserAreas, updateBusinessUserAreas } = require('../controllers/businessAreaController');
-const { authenticateToken } = require('../middleware/auth');
+const { authenticate } = require('../middleware/auth');
 
-// 獲取用戶的業務區域
-// Get user's business areas
+// 所有路由都需要驗證 / All routes require authentication
+router.use(authenticate);
+
+// 獲取用戶的業務區域 / Get user's business areas
 router.get('/:userId/areas', getBusinessUserAreas);
 
-// 更新用戶的業務區域
-// Update user's business areas
+// 更新用戶的業務區域 / Update user's business areas
 router.put('/:userId/areas', updateBusinessUserAreas);
 
 module.exports = router; 
